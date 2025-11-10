4e8b73a5922f8517b425c03b176cda7b21102676
vipin760 <vipinm500@gmail.com>
2025-11-10 12:12:43 +0530

    parents payment added

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -12,7 +12,6 @@ exports.createOrder = async (req, res) => {
         const studentData = await studentModel.findOne({ user_id: studentId })
         const shortReceipt = `order_${studentData.registration_number}_${Date.now().toString().slice(-6)}`;
         const order = await createOrder(amount, shortReceipt);
-        console.log("<><>order", order)
         const transaction = new Transaction({
             student_id: studentId,
             order_id: order.id,
@@ -47,7 +46,6 @@ exports.verifyPayment = async (req, res) => {
             { payment_id: razorpay_payment_id, status: 'paid' },
             { new: true }
         );
-console.log("<><>studentId",studentId)
         await userModel.findByIdAndUpdate(studentId, {
             subscription: true,
             subscriptionStart: new Date(),
@@ -64,3 +62,55 @@ console.log("<><>studentId",studentId)
         res.status(500).json({ success: false, message: 'Payment verification failed' });
     }
 };
+
+// parent make payment
+exports.parentCreatePayment = async(req, res)=>{
+       try {
+        const { studentId, amount } = req.body;
+        const studentData = await studentModel.findOne({ user_id: studentId })
+        const shortReceipt = `order_${studentData.registration_number}_${Date.now().toString().slice(-6)}`;
+        const order = await createOrder(amount, shortReceipt);
+        const transaction = new Transaction({
+            student_id: studentId,
+            order_id: order.id,
+            amount,
+            user_id: studentData.user_id
+        });
+        await transaction.save();
+
+        res.status(200).json({ success: true, order });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Order creation failed' });
+    }
+}
+
+exports.parentVerifyPayment = async (req, res) => {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature,studentId } = req.body;
+
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+            .update(body.toString())
+            .digest("hex");
+
+        if (expectedSignature !== razorpay_signature) {
+            return res.status(400).json({ success: false, message: "Invalid signature" });
+        }
+
+        const transaction = await Transaction.findOneAndUpdate(
+            { order_id: razorpay_order_id },
+            { payment_id: razorpay_payment_id, status: 'paid' },
+            { new: true }
+        );
+        await Student.findByIdAndUpdate(transaction.student_id, {
+            $inc: { wallet_balance: transaction.amount }
+        });
+
+        res.json({ success: true, message: "Payment verified and wallet updated" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Payment verification failed' });
+    }
+};
+
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createOrder, verifyPayment } = require('../controllers/payment.controller');
+const { createOrder, verifyPayment, parentCreatePayment, parentVerifyPayment } = require('../controllers/payment.controller');
 const router = express.Router();
 
 router.post("/create",createOrder);
 router.post("/verify",verifyPayment);
+
+router.post("/parent/create",parentCreatePayment)
+router.post("/parent/create",parentVerifyPayment)
 
 
 module.exports = router;
